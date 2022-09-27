82f034aa8ccfc419683ecced692514b3c6659ac5
Yago Neves <yagolife1@gmail.com>
2022-09-27 11:16:28 -0300

    Abot view

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,26 @@
 package gui;
 
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
@@ -17,6 +32,9 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	private MenuItem menuItemAbout;
+	
+	@FXML
+	private Hyperlink pageLink;
 
 	@FXML
 	public void onMenuItemSellerAction() {
@@ -30,12 +48,36 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/About.fxml");
+	}
+	
+	@FXML
+	public void onLinkItemPageAction(ActionEvent event) throws URISyntaxException, IOException{
+		
+		Desktop.getDesktop().browse(new URI("https://www.instagram.com/yago_neves/"));
 	}
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 
+	}
+
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+
+			Scene mainScene = Main.getMainScene();
+			VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVbox.getChildren().get(0);
+			mainVbox.getChildren().clear();
+			mainVbox.getChildren().add(mainMenu);
+			mainVbox.getChildren().addAll(newVBox.getChildren());
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module javafx.jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires java.desktop;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.graphics, javafx.fxml;
