3530b587a7ff1cbf60f2e1d8fcae6be1a92cd43d
Yago Neves <yagolife1@gmail.com>
2022-09-28 08:31:21 -0300

    Adding database access

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module javafx.jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires java.desktop;
+	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.graphics, javafx.fxml;
